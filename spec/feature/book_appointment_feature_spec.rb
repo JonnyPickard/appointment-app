7861ce51141841399c5dceed443a0bf134bfad41
Jonny Pickard <jonny@jonnys-mbp.home>
2016-11-20 20:12:44 +0000

    Update book_appointment_feature_spec
    Add test for no available times left to book
    Refactor book_appointment_feature_spec

--- spec/feature/book_appointment_feature_spec.rb
+++ spec/feature/book_appointment_feature_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 require_relative '../../lib/book_appointment.rb'
 
-describe BookAppointment, "#book" do
+describe BookAppointment, "#book\n" do
 
   book_appointment = described_class.new
 
@@ -11,7 +11,7 @@ describe BookAppointment, "#book" do
   end
 
   context "User can successfully book an available appointment" do
-    it "successfully prints back the valid inputted time confirming the booking" do
+    it "Successfully prints back the valid input time confirming the booking" do
       time = "12:30"
 
       expect { book_appointment.book(time) }.to output("12:30").to_stdout
@@ -19,21 +19,23 @@ describe BookAppointment, "#book" do
   end
 
   context "User cannot book appointment after 15:00 or before 08:00" do
-    it "successfully prints the next available appointment slot when given an invalid early time" do
+    error_message = "Error invalid time: Input time must be between '00:00' and '24:00' formatted as 'HH:MM' e.g. '13:50'"
+
+    it "Returns an error message when given an invalid early time" do
       time = "07:50"
 
-      expect { book_appointment.book(time) }.to output("08:00").to_stdout
+      expect { book_appointment.book(time) }.to output(error_message).to_stdout
     end
 
-    it "successfully prints the next available appointment slot when given an invalid late time" do
+    it "Returns an error message when given an invalid late time" do
       time = "15:50"
 
-      expect { book_appointment.book(time) }.to output("08:00").to_stdout
+      expect { book_appointment.book(time) }.to output(error_message).to_stdout
     end
   end
 
   context "User cannot book unavailable appointment slot" do
-    it "successfully prints the next available appointment slot when given an already booked time" do
+    it "Successfully prints the next available appointment slot when given an already booked time" do
       time = "08:00"
 
       2.times do
@@ -66,6 +68,19 @@ describe BookAppointment, "#book" do
       error_message  = "Error invalid time format: Input time must be formatted as 'HH:MM' e.g. '13:50'"
 
       expect { book_appointment.book(invalid_time_format_2) }.to output(error_message).to_stdout
+    end
+  end
+
+  context "User cannot book an appointment if there are no available slots left" do
+    it "Returns an error prompt when there are no more available appointment slots" do
+      time = "08:00"
+      error_message = "Error no appointments available: Please try again tomorrow"
+
+      56.times do
+        book_appointment.book(time)
+      end
+
+      expect { book_appointment.book(time) }.to output(error_message).to_stdout
     end
   end
 end
