c850bc7b651963c637997d953549f672e2174954
Jonny Pickard <jonny@jonnys-mbp.home>
2016-11-20 19:07:57 +0000

    Update book_appointment_feature_spec
    Add tests for invalid times after 15:00 and before 08:00

--- spec/feature/book_appointment_spec.rb
+++ spec/feature/book_appointment_spec.rb
@@ -7,11 +7,23 @@ describe BookAppointment, "#book" do
   book_appointment = described_class.new
 
   context "User can successfully book an available appointment" do
-
     it "successfully prints back the valid inputted time confirming the booking" do
       time = "12:30"
 
       expect { book_appointment.book(time) }.to output("12:30").to_stdout
+    end
+  end
+
+  context "User cannot book appointment after 15:00 or before 08:00" do
+    it "successfully prints next available appointment when given an invalid early time" do
+      time = "07:50"
+
+      expect { book_appointment.book(time) }.to output("08:00").to_stdout
+
+    it "successfully prints next available appointment when given an invalid late time" do
+      time = "15:50"
+
+      expect { book_appointment.book(time) }.to output("08:00").to_stdout
     end
   end
 end
